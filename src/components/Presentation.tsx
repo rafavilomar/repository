664e67f367c91fca131e401b3a512b7487d1602e
Rafael Vilomar <rafavilomar@gmail.com>
2021-03-19 23:37:54 -0400

    feat: Presentation added

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React from "react";
+
+import '../assets/styles/components/presentation.scss'
+import Button from "./Button";
+
+import {
+    CodeWorking
+} from 'react-ionicons'
 
 const Presentation = () => {
-    return (
-        <div>Presentation</div>
-    )
-}
+  return (
+    <section className="presentation">
+      <div className="presentation__nickname">
+        <h1 className="txt mainTitle-primary">@rafavilomar</h1>
+      </div>
+      <div className="presentation__details">
+        <p className="txt body">
+          I'm Rafael, a Web Developer and this is my personal corner on the Web
+          City. Here you can find information about me, how contact me and
+          explore my repositories.
+        </p>
+        <div>
+            <Button value="369labs" icon={<CodeWorking/>} variant='secondary'/>
+            <Button value="369labs" icon={<CodeWorking/>} variant='secondary'/>
+            <Button value="369labs" icon={<CodeWorking/>} variant='secondary'/>
+        </div>
+      </div>
+      <div className="presentation__action">
+          <Button value="Projects" />
+      </div>
+    </section>
+  );
+};
 export default Presentation;
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import Button from "../components/Button";
 import Header from "../components/layout/Header";
 
-import { Mail } from "react-ionicons/lib";
-import IconButton from "../components/IconButton";
 import ShortButton from "../components/layout/ShortButton";
+import Presentation from "../components/Presentation";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <IconButton icon={<Mail />} variant='primary'/>
-      <ShortButton/>
-      <h1 className="txt mainTitle-primary">@rafavilomar </h1>
-      <p className="txt body">I'm Rafael, a Web Developer and this is my personal corner on the Web City. Here you can find information about me, how contact me and explore my repositories.</p>
+      <ShortButton />
+      <Presentation />
+      <p style={{position: 'relative', top: '100vh'}}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod eaque
+        dolorum minus doloribus mollitia eius repellendus sed. Dolorem earum
+        laudantium laborum dolore, quam, unde corrupti, non voluptate cupiditate
+        quos fugit.
+      </p>
     </>
   );
 };
