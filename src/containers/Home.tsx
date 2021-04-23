c065539a3b67152ed7498d41bedd01f8186ecebb
Rafael <rafavilomar@gmail.com>
2021-04-22 23:30:40 -0400

    Social network added to Contacme, ShortButton hidden and button to download cv added on page About

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -6,7 +6,6 @@ import BigCardProject from "../components/BigCardProject";
 import Contacme from "../components/layout/Contacme";
 
 import Header from "../components/layout/Header";
-import ShortButton from "../components/layout/ShortButton";
 import Presentation from "../components/Presentation";
 import Skills from "../components/Skills";
 
@@ -21,7 +20,6 @@ const Home = () => {
   return (
     <>
       <Header />
-      <ShortButton />
       <Presentation />
       <div style={{ position: "relative", top: "100vh" }}>
         <Skills />
